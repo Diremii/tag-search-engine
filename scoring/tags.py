c2a526849992fd0a792f0bb800c4aed0e2844545
Hugo M <144733665+Diremii@users.noreply.github.com>
2026-07-10 11:02:15 +0200

    fix: add missing newline

--- scoring/tags.py
+++ scoring/tags.py
@@ -14,6 +14,7 @@ from utils.matching import score_words
 from utils.stemmer import stem_word
 from utils.config import LANGUAGES
 
+
 def score_page(query_words: list[str], page_tags: list[str]) -> float:
 	page_tags_stemmed = [stem_word(tag, LANGUAGES) for tag in page_tags]
 	return score_words(query_words, page_tags_stemmed)
